365fbfd7039f4b8877680314c3c9b102cafedc28
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-02 14:01:39 -0600

    Create 02_distinct.sql

---
+++ 02_distinct.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT * FROM users;
+"It olny provides distinct fields from table users."
+  
+SELECT DISTINCT age FROM users;
+"It only provides distinct age fields from table users."
+
+SELECT DISTINCT name FROM users;
+"It only provides distinct name fields from table users."

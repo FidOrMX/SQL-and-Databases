0f7d8a3ebae79208839aadf53c7813f4132c3e7e
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-03 10:44:55 -0600

    Update 05_like.sql

--- 05_like.sql
+++ 05_like.sql
@@ -1,7 +1,7 @@
 SELECT * FROM users WHERE email LIKE '%gmail.com';
-"Selects all users who's email ends with (gmail.com)"
+--Selects all users who's email ends with (gmail.com).
 
 SELECT * FROM users WHERE email LIKE 'sara%';
 
 SELECT * FROM users WHERE email LIKE '%@%';
-"Selects all users who's email posses an (@) symbol"
+--Selects all users who's email posses an (@) symbol.

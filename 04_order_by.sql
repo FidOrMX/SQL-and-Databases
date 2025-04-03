028e807946946625f79157bacd3da0df15176a2d
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-03 10:44:28 -0600

    Update 04_order_by.sql

--- 04_order_by.sql
+++ 04_order_by.sql
@@ -1,14 +1,14 @@
 SELECT * FROM users ORDER BY age;
-"It provides the users age in a descending order by default."
+--It provides the users age in a descending order by default.
 
 SELECT * FROM users ORDER BY age ASC;
-"It provides the users age in an ascending order."
+--It provides the users age in an ascending order.
 
 SELECT * FROM users ORDER BY age DESC;
-"It provides the users age in a descending order."
+--It provides the users age in a descending order.
 
 SELECT * FROM users WHERE email='sara@gmail.com' ORDER BY age DESC;
-"Selects email='sara@gmail.com' and orders by age in a descending way."
+--Selects email='sara@gmail.com' and orders by age in a descending way.
 
 SELECT name FROM users WHERE email='sara@gmail.com' ORDER BY age DESC;
-"Selects users name who posses the following email='sara@gmail.com' and orders by age in a descending way."
+--Selects users name who posses the following email='sara@gmail.com' and orders by age in a descending way.

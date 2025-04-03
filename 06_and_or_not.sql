b4c9a66f5af880d5faa1c9e2a46922fb4a76cca2
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-03 10:45:21 -0600

    Update 06_and_or_not.sql

--- 06_and_or_not.sql
+++ 06_and_or_not.sql
@@ -1,8 +1,8 @@
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com';
-"Selects all users except the ones with the email (sara@gmail.com)"
+--Selects all users except the ones with the email (sara@gmail.com).
 
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com' AND age = 15;
-"Selects all users except the ones with the email (sara@gmail.com) and who's age is 15."
+--Selects all users except the ones with the email (sara@gmail.com) and who's age is 15.
 
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com' OR age = 15;
-"Selects all users except the ones with the email (sara@gmail.com) or those who are 15 years old."
+--Selects all users except the ones with the email (sara@gmail.com) or those who are 15 years old.

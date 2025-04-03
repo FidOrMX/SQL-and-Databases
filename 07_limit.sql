582dafa514d28f45a3950e1c397855465af06fa4
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-03 10:45:42 -0600

    Update 07_limit.sql

--- 07_limit.sql
+++ 07_limit.sql
@@ -1,5 +1,5 @@
 SELECT * FROM users LIMIT 3;
-"Selects the first three users of our table."
+--Selects the first three users of our table.
 
 SELECT * FROM users WHERE NOT email = 'sara@gmail.com' OR age = 15 LIMIT 2;
-"Selects the users who's email is not 'sara@gmail.com' or who's age is 15 limitting itself withing the first 2 users of our table."
+--Selects the users who's email is not 'sara@gmail.com' or who's age is 15 limitting itself withing the first 2 users of our table.

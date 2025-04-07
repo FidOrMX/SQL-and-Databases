f0cc03336ac8cacce2e4bf939fcbf7f458fcdcd2
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-07 11:33:53 -0600

    Update 16_group_by.sql

--- 16_group_by.sql
+++ 16_group_by.sql
@@ -1,9 +1,2 @@
-SELECT CONCAT(name, surname) FROM users
---One obtains a columns containing the combination of the name and surname of each user.
-
-SELECT CONCAT('Nombre: ', name, ', Apellidos: ', surname) FROM users
---One obtains a columns containing the combination of the name and surname of each user. The sentence begins with "Nombre".
-
-SELECT CONCAT('Nombre: ', name, ', Apellidos: ', surname) AS 'Nombre completo' FROM users
---The table's name will now be 'Nombre completo' and not CONCAT.
-
+SELECT MAX(age) FROM users GROUP BY age
+--

a5d41dee59b2a38b4db2f3f476e181d7095023ca
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-13 16:02:50 -0600

    Delete 08_null.sql

--- 08_null.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM users WHERE email IS NULL;
---Quiero obtener todos los usuarios que tienen un correo nulo.
-
-SELECT * FROM users WHERE email IS NOT NULL;
---Quiero obtener todos los usuarios que no tienen un correo nulo.
-
-SELECT * FROM users WHERE email IS NOT NULL AND age = 15;
---Quiero obtener todos los usuarios que no tienen un correo nulo y cuya edad sea 15.

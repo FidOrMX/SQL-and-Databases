28e0e1563d09b4f6309e8248bf248d46c5f2bf9c
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-13 12:03:41 -0600

    Delete 02_distinct.sql

--- 02_distinct.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT * FROM users;
---It olny provides distinct fields from table users.
-  
-SELECT DISTINCT age FROM users;
---It only provides distinct age fields from table users.
-
-SELECT DISTINCT name FROM users;
---It only provides distinct name fields from table users.

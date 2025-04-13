cf43924297b8b3692f21edc74d0c35b08fc89159
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-13 16:30:12 -0600

    Create 10_count.sql

---
+++ Modifiers/10_count.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(*) FROM users;
+--Counts number of users inside the table.
+
+SELECT COUNT(age) FROM users;
+--Counts number of users who have a determined age, without counting the ones marked with null information.

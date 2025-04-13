0570c74db0b698b0dd8e1f7b0bde4fd97bb34ea0
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-13 16:32:48 -0600

    Delete 10_count.sql

--- 10_count.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) FROM users;
---Counts number of users inside the table.
-
-SELECT COUNT(age) FROM users;
---Counts number of users who have a determined age, without counting the ones marked with null information.

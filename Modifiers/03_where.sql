99f022a1b2b5eda8c7897b9e780deb534c5b3112
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-13 12:04:35 -0600

    Create 03_where.sql

---
+++ Modifiers/03_where.sql
@@ -0,0 +1,11 @@
+SELECT FROM users WHERE age = 15;
+--It only provides the users whose age is equal to 15.
+
+SELECT name FROM users WHERE age = 15;
+--It only provides the users name whose age is equal to 15.
+
+SELECT DISTINCT name FROM users WHERE age = 15;
+--It only provides the users distinct name whose age is equal to 15.
+
+SELECT DISTINCT age FROM users WHERE age = 15;
+--It only provides the users distinct age whose age is equal to 15.

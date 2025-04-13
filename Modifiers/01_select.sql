48196a789e1799ffff67aa104ff488cf692f91fc
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-13 11:49:24 -0600

    Create 01_select.sql

---
+++ Modifiers/01_select.sql
@@ -0,0 +1,9 @@
+SELECT * FROM users
+--Selects the whole table users.
+--Remark: one has to always finish with (;) or else the code will not function, except for the case in which the whole table has to be selected.
+  
+SELECT name FROM users;
+--Selects the columns name from table users.
+
+SELECT user_id, name FROM users;
+--Selects column user_id from table users.

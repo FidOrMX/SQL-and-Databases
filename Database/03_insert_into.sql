e0984f2af61aa5dac52ad65977c293a042936da5
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-17 10:18:25 -0600

    Update 03_insert_into.sql

--- Database/03_insert_into.sql
+++ Database/03_insert_into.sql
@@ -1,2 +1,8 @@
 INSERT INTO users (user_id, name, surname) VALUES (8, 'Maria', 'Lopez')
---Insert a new column inside my table "users".
+--Inserts a new (user_id) with number (8) and adds the values (Maria, Lopez) to the entries (name, surname).
+
+INSERT INTO users (name, surname) VALUES ('Paco', 'Pérez')
+--Autoincrement allows me to discard the user_id.
+
+INSERT INTO users (user_id, name, surname) VALUES (11, 'El', 'Lopez')
+--Jumps up to the user_id value 11.

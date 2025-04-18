6521b112b5190aa3518edab619c2e7e3a55f56ba
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-18 16:00:24 -0600

    Update and rename aaaa to 01_create_tables.sql

---
+++ Tables/01_create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE persons (
+    id int,
+    name varchar(100),
+    age int,
+    email varchar(50),
+    created date
+  ):
+--This is the command for creating a table named "persons" inside a new created data base. The string values are specified.

5ed15c879ee7d56e59f91a83bca8532ae6be86d0
Fidel Ortega Hernández <fidel.ohem@gmail.com>
2025-04-21 09:10:39 -0600

    Create Aqui!!!03_unique.sql

---
+++ Tables/Aqui!!!03_unique.sql
@@ -0,0 +1,8 @@
+CREATE TABLE persons2 (
+    id int NOT NULL,
+    name varchar(100) NOT NULL,
+    age int,
+    email varchar(50),
+    created date
+  ):
+--This is the command for creating a table named "persons2" inside a new created data base. The string values are specified. "NOT NULL" forces the user to enter a specific value.
